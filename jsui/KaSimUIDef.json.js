f607430661cfcc4c1cd13f1e96c84ac38467d56b
Michael D Pedersen <michael.d.pedersen@gmail.com>
2013-05-01 21:16:35 +0100

    Now with the KaSim js wrapper.

--- jsui/KaSimUIDef.json.js
+++ jsui/KaSimUIDef.json.js
@@ -1,7 +1,7 @@
 ﻿jsonCallback({
     "name": "KaSim", 
     "messageFormat": "1", 
-    "simulatorURL": "https://raw.github.com/NicolasOury/KaSimJS/master/KaSimWrapper.js",
+    "simulatorURL": "https://raw.github.com/NicolasOury/KaSimJS/master/jsui/KaSimWrapper.js",
     "defaultModelURL": "https://raw.github.com/NicolasOury/KaSimJS/master/jsui/abc.ka.js",
     "exampleModels": 
         {"ABC": "https://raw.github.com/NicolasOury/KaSimJS/master/jsui/abc.ka.js"},
