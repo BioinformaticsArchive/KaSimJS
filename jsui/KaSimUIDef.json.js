781f41f817c2349ae93ee2311cc6fa7deddeb61c
Michael D Pedersen <michael.d.pedersen@gmail.com>
2013-05-01 20:46:50 +0100

    Added the js-encoded sample abs.ka model... now with url error fixed once more

--- jsui/KaSimUIDef.json.js
+++ jsui/KaSimUIDef.json.js
@@ -2,9 +2,9 @@
     "name": "KaSim", 
     "messageFormat": "1", 
     "simulatorURL": "https://raw.github.com/NicolasOury/KaSimJS/master/KaSim.js",
-    "defaultModelURL": "https://raw.github.com/NicolasOury/KaSimJS/master/jsui/abs.ka.js",
+    "defaultModelURL": "https://raw.github.com/NicolasOury/KaSimJS/master/jsui/abc.ka.js",
     "exampleModels": 
-        {"ABC": "https://raw.github.com/NicolasOury/KaSimJS/master/jsui/abs.ka.js"},
+        {"ABC": "https://raw.github.com/NicolasOury/KaSimJS/master/jsui/abc.ka.js"},
     "keywords": ["init", "obs", "agent"],
     "symbols": ["%", "~", "\\?", "!", ",", "->"],
     "delimitors": []
