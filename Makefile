2c247d5c0efa0651444be456daea01496647f1b8
Nicolas Oury <Nicolas.Oury@gmail.com>
2013-05-01 11:05:56 +0100

    begining of protocol

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-all: jsLibs.js main.js
+all: KaSim.js
 
-jsLibs.js : 
-	ocamlbuild -use-ocamlfind js/jsLib.byte
-	js_of_ocaml -noruntime  _build/js/jsLib.byte
+KaSim.js: jsWorkerProtocol.js main.js
+	cat js/runtime.js js/header.js _build/js/jsWorkerProtocol.js js/spliceCallbacks.js _build/main/main.js > KaSim.js
+
+
+jsWorkerProtocol.js : 
+	ocamlbuild -use-ocamlfind js/jsWorkerProtocol.byte
+	js_of_ocaml -noruntime  _build/js/jsWorkerProtocol.byte
 main.js : 
 	ocamlbuild -use-ocamlfind main/main.byte
 	js_of_ocaml -noruntime  _build/main/main.byte
---
+++ js/jsWorkerProtocol.ml
@@ -0,0 +1,38 @@
+open JsLib
+
+
+let message_back = 
+  let split_space s = 
+    Regexp.split (Regexp.regexp " ") s in
+  let array_of_list l =  
+    let a = jsnew Js.array_empty() in
+    List.iter (fun s ->  
+      ignore(a##push(Js.string s))) l;
+    a in
+  let parse_species_names s = 
+    let l = split_space s in
+    match l with 
+    | "#"::"time"::l ->
+      array_of_list l 
+    | _ -> assert false in
+  let species_sent = ref false in
+  let empty_array = jsnew Js.array_empty() in
+  let species = ref None in
+  fun s -> 
+    match !species with
+      None -> species := Some (parse_species_names s)
+    | Some a -> 
+     	let species_array = if !species_sent then empty_array
+	  else (species_sent := true;  a) in
+	let l = split_space s in
+	match l with 
+	  time :: spec -> assert (String.length time > 0);
+	    let open Js.Unsafe in
+	    let msg = 
+	       obj [|("time", inject time);
+		     ("newSpec", inject species_array);
+		     ("newData", inject (array_of_list spec));
+		     ("isComplete", inject false) |] in
+	   ignore(fun_call  (variable "processMessageCallback") [|msg|])
+  
+let _ = line_callback "output" message_back
