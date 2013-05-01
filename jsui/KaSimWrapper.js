8d9082ad6c0c7c2be1d0c4ec46ccd877563d519e
Michael D Pedersen <michael.d.pedersen@gmail.com>
2013-05-01 21:13:37 +0100

    Now with the KaSim js wrapper.

---
+++ jsui/KaSimWrapper.js
@@ -0,0 +1,34 @@
+﻿var processMessageCallback = null;
+var inputFile = null;
+var numEventsStr = null;
+
+var Simulator = (function () {
+
+    return {
+
+        simulate: function(progStrPar, eventsPar, processMessageCallbackPar) {
+
+            try {
+                // will start simulation and callback until complete.
+                processMessageCallback = processMessageCallbackPar;
+                inputFile = progStrPar;
+                numEventsStr = eventsPar.toString();
+
+                importScripts("../KaSim/KaSim.js");
+
+                // need to provide the final "isComplete" callback here:
+                var message = { isComplete: true };
+                processMessageCallback(message);
+
+                return false;
+            }
+            catch (e) {
+                var message = { isComplete: true, errMsg: e.message };
+                self.postMessage(message);
+            }
+        },
+        
+        stop: function () {},
+    };
+
+    })();
