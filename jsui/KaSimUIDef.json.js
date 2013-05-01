f952170dbda7ef8f09b85aa0ed0c7d6463585570
Michael D Pedersen <michael.d.pedersen@gmail.com>
2013-05-01 14:19:59 +0100

    Added a ui simulator def file.

---
+++ jsui/KaSimUIDef.json.js
@@ -0,0 +1,11 @@
+﻿{
+    "name": "KaSim", 
+    "messageFormat": "1", 
+    "simulatorURL": "https://raw.github.com/jkrivine/KaSim/master/jsui/KaSim.js",
+    "defaultModelURL": "https://raw.github.com/jkrivine/KaSim/master/models/abc.ka",
+    "exampleModels": 
+        {"ABC": "https://raw.github.com/jkrivine/KaSim/master/models/abc.ka"},
+    "keywords": ["init", "obs", "agent"],
+    "symbols": ["%", "~", "\\?", "!", ",", "->"],
+    "delimitors": []
+}
