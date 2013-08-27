a76b3ca82fb88968a94b2dba20fa49f005c0e0b6
michaeldavidpedersen <michael.d.pedersen@gmail.com>
2013-08-27 10:14:58 +0100

    github raw URLs now go through appengine proxy to create appropriate content type - fix

--- jsui/KaSimJSON.js
+++ jsui/KaSimJSON.js
@@ -4,10 +4,10 @@
     "messageFormat": "1",
     "runInWorker": true,
     "useJSONP": true,
-    "simulatorURL": "http://mdp-fileserver2.appspot.com/proxy?f=KaSimWrapper.js",
-    "defaultModelURL": "http://mdp-fileserver2.appspot.com/proxy?f=KaSimEx.js",
+    "simulatorURL": "http://mdp-fileserver2.appspot.com/proxy?f=jsui/KaSimWrapper.js",
+    "defaultModelURL": "http://mdp-fileserver2.appspot.com/proxy?f=jsui/KaSimEx.js",
     "exampleModels":
-    { "ABC": "http://mdp-fileserver2.appspot.com/proxy?f=KaSimEx.js" },
+    { "ABC": "http://mdp-fileserver2.appspot.com/proxy?f=jsui/KaSimEx.js" },
     "keywords": ["init", "obs", "agent", "simulate"],
     "symbols": ["%", "~", "\\?", "!", ",", "->"],
     "delimitors": []
